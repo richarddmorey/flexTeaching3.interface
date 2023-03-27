0d6bdb3673e76626ce989971ea7ee8ea65fd7698
Richard Morey <richarddmorey@gmail.com>
2023-03-27 17:28:06 +0100

    Update start_interface.R

--- inst/start_interface.R
+++ inst/start_interface.R
@@ -2,7 +2,7 @@
 library(flexTeaching3.api)
 
 
-flexTeaching3.api::ft3_options(assignments_pkg = 'flexTeaching3.cardiff')
+flexTeaching3.api::ft3_options(assignments_pkg = 'flexTeaching3.api')
 
 flexTeaching3.interface::ft3_serve_interface(
   api_location = 'http://localhost:8080',
