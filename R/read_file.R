f25e8abf71abf54043f77812c8143dda4f634457
Richard Morey <richarddmorey@gmail.com>
2023-05-04 08:10:47 +0100

    fix apparent bug with reading UTF8 encoded files

--- R/read_file.R
+++ R/read_file.R
@@ -1,10 +1,14 @@
 
-ft3_read_file_text <- function(path){
+ft3_read_file_text <- function(path, ...){
   if(!file.exists(path))
     stop('File ', path, ' not found.')
   to.read <- file(path, "rb")
   on.exit({
     close(to.read)
   })
-  readChar(to.read, file.size(path))
+  # See https://github.com/r-lib/rcmdcheck/issues/152
+  # readChar(to.read, file.size(path))
+  readChar(to.read, nchars = file.size(path), useBytes = TRUE, ...) |>
+    enc2utf8()
 }
+
